41e5e8026cef1142bf490a63440ea908a9ed66cc
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-02-27 19:56:43 +0100

    Write sheleton of View class

---
+++ View.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.Map;
+
+public class View {
+    public void print(Number num){
+
+    }
+
+    public void print(String str){
+
+    }
+
+    public void print(List<String> list){
+
+    }
+
+    public void print(Map<String, Integer> map, int i){
+
+    }
+}
