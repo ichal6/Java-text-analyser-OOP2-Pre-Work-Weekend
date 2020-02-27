c80e0f4e7159c224df9efd8e9d0daae6d7391c53
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-02-27 19:57:31 +0100

    Write sheleton of StatisticalAnalysis class

---
+++ StatisticalAnalysis.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class StatisticalAnalysis {
+    public Iterator<String>  statisticalAnalysis;
+
+    public int countOf(String... elems){
+        return 0;
+    }
+
+    public int dictionarySize(){
+        return 0;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public Set<String> occurMoreThan(Integer n){
+        Set<String> newSet = new HashSet<String>();
+
+        return newSet;
+    }
+}
