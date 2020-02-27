31bfd76f772df7946038fc6d4428b503dab4a6aa
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-02-27 19:58:18 +0100

    Write WordIterator and CharIterator subclasses

---
+++ CharIterator.java
@@ -0,0 +1,18 @@
+import java.util.Iterator;
+
+public class CharIterator implements Iterator {
+    public FileContent charIterator;
+
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public String next() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
---
+++ WordIterator.java
@@ -0,0 +1,19 @@
+import java.util.Iterator;
+
+public class WordIterator implements Iterator {
+    public FileContent wordIterator;
+
+
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public String next() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
