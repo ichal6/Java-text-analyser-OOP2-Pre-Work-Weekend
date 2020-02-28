6a4b1018d74f2f0f6957bb3a5d9d58c45ea5cd25
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-02-28 22:59:50 +0100

    Move build String from CharIterator to FileContent

--- src/FileContent.java
+++ src/FileContent.java
@@ -7,9 +7,17 @@ public class FileContent implements IterableText {
     private String filename;
     public String fileContent;
 
-    FileContent(){
+    FileContent(boolean toCharIterator){
         this.fileContent = "This is example string It is example text in this String";
         this.fileContent = this.fileContent.toLowerCase();
+        if(toCharIterator){
+            String[] splitString = fileContent.split(" ");
+            StringBuffer sb = new StringBuffer();
+            for(int i = 0; i < splitString.length; i++) {
+                sb.append(splitString[i]);
+            }
+            fileContent = sb.toString();
+        }        
     }
 
     @Override
