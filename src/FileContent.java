6620c12a75e4539df17bbadcbc3224429aed7f9b
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-02-28 22:49:22 +0100

    Implement FileContent class

--- src/FileContent.java
+++ src/FileContent.java
@@ -7,16 +7,23 @@ public class FileContent implements IterableText {
     private String filename;
     public String fileContent;
 
+    FileContent(){
+        this.fileContent = "This is example string It is example text in this String";
+        this.fileContent = this.fileContent.toLowerCase();
+    }
+
     @Override
     public Iterator<String> charIterator() {
+        Iterator<String> newIterator = new CharIterator(); 
         // TODO Auto-generated method stub
-        return null;
+        return newIterator;
     }
 
     @Override
     public Iterator<String> wordIterator() {
+        Iterator<String> newIterator = new WordIterator(); 
         // TODO Auto-generated method stub
-        return null;
+        return newIterator;
     }
 
     public String getFilename(){
