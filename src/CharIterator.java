45d6dbb600726ef92756a490f8d5e8501ff34d87
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-02-28 20:18:27 +0100

    Repair method next in CharIterator

--- src/CharIterator.java
+++ src/CharIterator.java
@@ -16,6 +16,11 @@ public class CharIterator implements Iterator {
     @Override
     public String next() {
         String[] splitString = fromFile.split("");
+        StringBuffer sb = new StringBuffer();
+        for(int i = 1; i < splitString.length; i++) {
+            sb.append(splitString[i]);
+        }
+        fromFile = sb.toString();
         return splitString[0];
     }
     
