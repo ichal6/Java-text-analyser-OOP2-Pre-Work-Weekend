26cb84421cdd09f2d4170bd5cb7d068b10fbbbad
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-03 18:57:36 +0100

    Add time calculate to Application

--- src/Application.java
+++ src/Application.java
@@ -2,6 +2,14 @@ package src;
 
 public class Application{
     public static void main(String[] args){
+        long startTime = System.nanoTime();
         new StatisticalAnalysis(args);
+        long endTime = System.nanoTime();
+        Double start = Double.valueOf(startTime);
+        Double end = Double.valueOf(endTime);
+        System.out.println((end - start)/1000000000.0 + " seconds");
+
     }
+
+
 }
