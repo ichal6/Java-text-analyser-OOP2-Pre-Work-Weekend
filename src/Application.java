1b3c3e7f3239b7e82f85fd6faf7e880b94e31239
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-03 20:02:31 +0100

    Restructure Application file

--- src/Application.java
+++ src/Application.java
@@ -2,14 +2,19 @@ package src;
 
 public class Application{
     public static void main(String[] args){
-        long startTime = System.nanoTime();
+        long startTime = startTime();
         new StatisticalAnalysis(args);
+        stopTime(startTime);
+    }
+
+    private static long startTime(){
+        return System.nanoTime();
+    }
+
+    private static void stopTime(long startTime){
         long endTime = System.nanoTime();
         Double start = Double.valueOf(startTime);
         Double end = Double.valueOf(endTime);
         System.out.println((end - start)/1000000000.0 + " seconds");
-
     }
-
-
 }
