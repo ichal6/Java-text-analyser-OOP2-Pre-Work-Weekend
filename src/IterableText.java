ffd4b696c6f33e26c3975b8cb1c64dfc0fa072b5
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-03-05 20:31:22 +0100

    Remove unuse throws from IterableText

--- src/IterableText.java
+++ src/IterableText.java
@@ -1,13 +1,11 @@
 package src;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Iterator;
 
 public interface IterableText{
 
-    public Iterator<String> charIterator() throws FileNotFoundException, NullPointerException, IOException ;
+    public Iterator<String> charIterator();
     
-    public Iterator<String> wordIterator() throws FileNotFoundException, NullPointerException, IOException ;
+    public Iterator<String> wordIterator();
 
 }
